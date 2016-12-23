8bcb28958c0f18dbfb297c297ba9bce06709a0a8
DeokJin Lee <truevirtue@nexell.co.kr>
2016-12-23 10:22:10 +0900

    bl1: startup code modify the location
    This patch has been modified to change the starting
    position in the startup code depending on the kernel
    version dependency. This is to provide normal operation
    for all kernels.
    
    Change-Id: If5e6249ae342592f0b2e345ba5e254c7be742ca1
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>

---
+++ src/s5p6818.h
@@ -0,0 +1,50 @@
+#ifndef __S5P6818_H__
+#define __S5P6818_H__
+
+#define CFG_NSIH_EN				1
+
+/* NXPXX30 - Chip Product */
+#define ARCH_NXP5430
+
+/* Support the Kernel Version */
+#define SUPPORT_KERNEL_3_4			1
+
+#if (SUPPORT_KERNEL_3_4 == 1)
+/* System Option */
+#define MULTICORE_SLEEP_CONTROL			1
+#define MULTICORE_BRING_UP			1
+#define CCI400_COHERENCY_ENABLE			1
+#define CONFIG_RESET_AFFINITY_ID		0				// Dependent kernel 3.18.x
+#else	// #if (SUPPORT_KERNEL_3_4 == 1)
+/* System Option */
+#define MULTICORE_SLEEP_CONTROL			1
+#define MULTICORE_BRING_UP			0
+#define CCI400_COHERENCY_ENABLE			1
+#define CONFIG_RESET_AFFINITY_ID		1				// Dependent kernel 3.18.x
+#endif
+
+/* DRAM(DDR3/LPDDR3) Memory Configuration */
+#ifdef MEMTYPE_DDR3
+#define MEM_TYPE_DDR3
+#endif
+#ifdef MEMTYPE_LPDDR3
+#define MEM_TYPE_LPDDR23
+#endif
+
+/* Serial Console Configuration */
+#define CONFIG_S5P_SERIAL
+#define CONFIG_S5P_SERIAL_INDEX			0
+#define CONFIG_S5P_SERIAL_CLOCK			50000000
+
+#define CONFIG_S5P_SERIAL_SRCCLK		0
+#define CONFIG_S5P_SERIAL_DIVID			4
+#define CONFIG_S5P_SERIAL_
+
+#define CONFIG_BAUDRATE				115200
+
+#define CONFIG_UART_CLKGEN_CLOCK_HZ		0
+
+/* CRC Check Configuration */
+#define SUPPORT_SW_CRC_CHECK			1
+
+#endif // __S5P6818_H__
---
+++ src/type.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: DeokJin, Lee <truevirtue@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __TYPE_H__
+#define __TYPE_H__
+
+#define IO_ADDRESS(x)			(x)
+
+#define mmio_read_32(addr)		(*(volatile unsigned int  *)(addr))
+#define mmio_read_16(addr)		(*(volatile unsigned short*)(addr))
+#define mmio_read_8(addr)		(*(volatile unsigned char *)(addr))
+
+#define mmio_write_32(addr,data)	(*(volatile unsigned int  *)(addr))  =  ((unsigned int  )(data))
+#define mmio_write_16(addr,data)	(*(volatile unsigned short*)(addr))  =  ((unsigned short)(data))
+#define mmio_write_8(addr,data)		(*(volatile unsigned char *)(addr))  =  ((unsigned char )(data))
+
+#define mmio_set_32(addr,data)		(*(volatile unsigned int  *)(addr)) |=  ((unsigned int  )(data))
+#define mmio_set_16(addr,data)		(*(volatile unsigned short*)(addr)) |=  ((unsigned short)(data))
+#define mmio_set_8(addr,data)		(*(volatile unsigned char *)(addr)) |=  ((unsigned char )(data))
+
+#define mmio_clear_32(addr,data)	(*(volatile unsigned int  *)(addr)) &= ~((unsigned int  )(data))
+#define mmio_clear_16(addr,data)	(*(volatile unsigned short*)(addr)) &= ~((unsigned short)(data))
+#define mmio_clear_8(addr,data)		(*(volatile unsigned char *)(addr)) &= ~((unsigned char )(data))
+
+#define readb(addr)			mmio_read_8(addr)
+#define readw(addr)			mmio_read_16(addr)
+#define readl(addr)			mmio_read_32(addr)
+
+#define writeb(data, addr)		({U8  *_v = (U8 *)addr;  mmio_write_8(_v, data);})
+#define writew(data, addr)		({unsigned short *_v = (unsigned short *)addr; mmio_write_16(_v, data);})
+#define writel(data, addr)		({unsigned int *_v = (unsigned int *)addr; mmio_write_32(_v, data);})
+
+#define u8				unsigned char
+#define u16				unsigned short
+#define u32				unsigned int
+#define u64				unsigned long long
+
+#define s8				char
+#define s16				short
+#define s32				int
+#define s64				long long
+#endif
+
